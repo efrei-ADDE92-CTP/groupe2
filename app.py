a982d0d56a5c0af09e49877fc432102f621e867a
EKANE <ekaneekane3@gmail.com>
2023-02-27 20:50:45 +0100

    [UPDATE] app file structure

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
+import joblib
+from flask import Flask, request, jsonify
+
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
-from flask import Flask, request, jsonify
-import joblib
+
 from prometheus_client import Counter, start_http_server
 
 # Load the Iris dataset
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
+
 # Split the dataset into a training set and a test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # Train a KNN model
 knn = KNeighborsClassifier()
 knn.fit(X_train, y_train)
+
 # Evaluate the model
 accuracy = knn.score(X_test, y_test)
 print('Test set accuracy: {:.2f}'.format(accuracy))
+
 # Make predictions on unseen data
 X_new = [[5.1, 3.5, 1.4, 0.2], [6.3, 3.3, 4.7, 1.6]]
 predictions = knn.predict(X_new)
 print('Predictions: {}'.format(predictions))
+
 scores = cross_val_score(knn, X, y, cv=5)
 training_score = knn.score(X_train, y_train)
 
@@ -36,24 +43,15 @@ if training_score > test_score:
 else:
     print('Model is not overfitting')
 
+# Make a dump of our model
 joblib.dump(knn, 'knn_model.pkl')
 
 
 app = Flask(__name__)
 
-
-if __name__ == "__main__":
-    start_http_server(8000) # Start the Prometheus HTTP server on port 8000
-    app.run(host="0.0.0.0")
-
-# Load the model from the file
-    model = joblib.load('knn_model.pkl')
-
-# Load the Iris dataset
-    iris = datasets.load_iris()
-
 # Create a Prometheus counter to count API calls
 api_calls_counter = Counter("api_calls", "Number of API calls")
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -71,4 +69,24 @@ def predict():
 
     # Return the prediction as a response
     return jsonify(species)
+
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    res = []
+    res.append(prometheus_client.generate_latest(c))
+    return Response(res, mimetype="text/plain")
+
+if __name__ == "__main__":
+    # Load the model from the file
+    model = joblib.load('knn_model.pkl')
+
+    # Load the Iris dataset
+    iris = datasets.load_iris()
+
+    # Start the Prometheus HTTP server on port 8000
+    start_http_server(8000) 
+
+    # Run our ap on localhost
+    app.run(host="0.0.0.0")
 
